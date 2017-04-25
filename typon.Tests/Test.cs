33233a55675ece78c0f85ef6763f761d1be6732e
cagaray <cgaray@gmail.com>
2017-04-25 00:44:19 -0700

    adding basic testing project

---
+++ typon.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace typon.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
