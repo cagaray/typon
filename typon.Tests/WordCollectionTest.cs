17567ce308c5aa41fc5fe9939f7f48d589f86233
cagaray <cgaray@gmail.com>
2017-04-26 15:54:36 -0700

    reading list of words from file

---
+++ typon.Tests/WordCollectionTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using typon;
+namespace typon.Tests
+{
+    [TestFixture()]
+    public class WordCollectionTest
+    {
+        [Test()]
+        public void getOneWordShouldGetOneWord()
+        {
+            WordCollection words = new WordCollection();
+            Assert.IsNotEmpty(words.getOneRandomWord());
+        }
+    }
+}
---
+++ typon/WordCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace typon
+{
+    public class WordCollection
+    {
+		Assembly assembly;
+		StreamReader textStreamReader;
+        //private string filePath = @"/Users/cgaray/Documents/mims_berkeley/2017_spring/info_290t_agile_engineering_practices/final_project/typon/typon/resources/words.txt";
+        private List<string> wordsInFile = new List<string>();
+        private Random randomGenerator;
+        private int numberOfWordsInFile;
+
+
+        public WordCollection()
+        {
+            assembly = Assembly.GetExecutingAssembly();
+            using (textStreamReader = new StreamReader(assembly.GetManifestResourceStream("typon.words.txt")))
+			{
+				string line;
+                while ((line = textStreamReader.ReadLine()) != null)
+				{
+                    wordsInFile.Add(line);
+				}
+                numberOfWordsInFile = wordsInFile.Count;
+			}
+            //wordsInFile = File.ReadAllLines(filePath);
+            //numberOfWordsInFile = wordsInFile.Length;
+            randomGenerator = new Random();
+        }
+          
+        public string getOneRandomWord()
+        {
+            //Console.WriteLine("word: {0}", wordsInFile[randomGenerator.Next(numberOfWordsInFile)]);
+            //Console.WriteLine("word: {0}", wordsInFile[randomGenerator.Next(numberOfWordsInFile)]);
+            return wordsInFile[randomGenerator.Next(numberOfWordsInFile)];
+        }
+    }
+}
